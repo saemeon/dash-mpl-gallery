440cc755723341c041c11d70558dc0acb1eca1c6
Simon Niederberger <s.niederberger@hotmail.com>
2026-03-22 15:57:07 +0100

    port dash_capture from s5ndt

---
+++ packages/dash-capture/src/dash_capture/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) Simon Niederberger.
+# Distributed under the terms of the MIT License.
+
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("dash-capture")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+from dash_fn_interact import Field, FieldHook, FromComponent
+from dash_capture._ids import id_generator
+from dash_capture.dropdown import build_dropdown
+from dash_capture.fig_export import FromPlotly, graph_exporter
+from dash_capture.wizard import Wizard, build_wizard
+
+__all__ = [
+    "Field",
+    "FieldHook",
+    "FromComponent",
+    "FromPlotly",
+    "Wizard",
+    "build_dropdown",
+    "build_wizard",
+    "graph_exporter",
+    "id_generator",
+]
